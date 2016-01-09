b8a7d761f2483fa009c9743a818b0c91a344073c
Jim Reed <ire@us-rem-ire.local>
2016-01-09 11:30:39 -0500

    added get all fretboard positions

--- fretboard.js
+++ fretboard.js
@@ -10,31 +10,60 @@ module.exports = {
 
     for (i = 0; i < frets.length; i++) {
       if (frets[i] > 0) {
-        cx = 0;
-        cy = 0;
+        position = note2position({ string:i , fret:frets[i]});
 
-        if ((i+1) % 2 == 0) {
-          cx = 15 * (i+1) / 2;
-        } else {
-          cx = 15 * i / 2 + 7;
-        }
-        if (frets[i] == 1) {
-          cy = 11;
-        }
-        if (frets[i] == 2) {
-          cy = 18;
-        }
-        if (frets[i] == 3) {
-          cy = 26;
-        }
-        if (frets[i] == 4) {
-          cy = 33;
-        }
-
-        positions.push({ cx:cx , cy:cy });
+        positions.push({ cx:position.cx , cy:position.cy });
       }
     }
     return positions;
-  }
+  },
+
+  getFretboard: function() {
+
+    positions = [];
+
+    for (f = 0; f < 4; f++) {
+      for (s = 0; s < 6; s++) {
+        position = note2position({ string:s , fret:f});
+
+        positions.push({ string:s , fret:f , cx:position.cx , cy:position.cy });
+      }
+    }
+
+    return positions;
+  },
+
+  selectFret: function(def, selected) {
+    if (selected.string < 0 || selected.string > 5) {
+      return def;
+    }
+    def.frets[selected.string] = selected.fret;
+
+    return def;
+  },
 
 };
+
+function note2position(note) {
+  position = { cx:0 , cy:0 };
+
+  if ((note.string+1) % 2 == 0) {
+    position.cx = 15 * (note.string+1) / 2;
+  } else {
+    position.cx = 15 * note.string / 2 + 7;
+  }
+  if (note.fret == 1) {
+    position.cy = 11;
+  }
+  if (note.fret == 2) {
+    position.cy = 18;
+  }
+  if (note.fret == 3) {
+    position.cy = 26;
+  }
+  if (note.fret == 4) {
+    position.cy = 33;
+  }
+
+  return position;
+}
--- tests/fretboard.js
+++ tests/fretboard.js
@@ -3,7 +3,7 @@ var should = require('chai').should(),
 
 describe('#fretboard', function() {
 
-  it('getFingerPositions', function() {
+  it('should return fret positions', function() {
 
     positions = fretboard.getFingerPositions({ frets:[-1,0,2,2,1,0] });
     positions.length.should.equal(3);
@@ -13,6 +13,40 @@ describe('#fretboard', function() {
     positions[0].cx.should.equal(22);
     positions[1].cx.should.equal(30);
     positions[2].cx.should.equal(37);
+  });
+
+  it('can select no frets', function() {
+    def = fretboard.selectFret({ frets:[-1,0,2,2,1,0] } , { string:-1 , fret:0 } );
+    def.frets.length.should.equal(6);
+    def.frets[0].should.equal(-1);
+    def.frets[1].should.equal(0);
+    def.frets[2].should.equal(2);
+    def.frets[3].should.equal(2);
+    def.frets[4].should.equal(1);
+    def.frets[5].should.equal(0);
+  });
+
+  it('can select a fret', function() {
+    def = fretboard.selectFret({ frets:[-1,0,2,2,1,0] } , { string:2 , fret:3 } );
+    def.frets.length.should.equal(6);
+    def.frets[0].should.equal(-1);
+    def.frets[1].should.equal(0);
+    def.frets[2].should.equal(3);
+    def.frets[3].should.equal(2);
+    def.frets[4].should.equal(1);
+    def.frets[5].should.equal(0);
+  });
+
+  it('should return the full fretboard', function() {
+    positions = fretboard.getFretboard();
+    positions[0].string.should.equal(0);
+    positions[0].fret.should.equal(0);
+    positions[0].cx.should.equal(7);
+    positions[0].cy.should.equal(0);
+    positions[6].string.should.equal(0);
+    positions[6].fret.should.equal(1);
+    positions[6].cx.should.equal(7);
+    positions[6].cy.should.equal(11);
   });
 
 });
